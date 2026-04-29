5fe064710bb2ebc1e3178f97ada68210431264ab
Andrea Michelle Olivas Rivera <andreaolivas@Andreas-MacBook-Pro-3.local>
2026-04-29 16:59:31 -0600

    mbackground

---
+++ background/background.js
@@ -0,0 +1,5 @@
+// Este es un simple script de fondo que puede usarse para escuchar eventos
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("KueskiPay Assistant Extension instalado");
+});
+
